4ac667634bbad3a860bea7e6ad673c5ad48d04c6
neow24 <neo.wide@metropolia.fi>
2025-09-10 21:31:30 +0300

    temperatuertest färdig

--- sm-maven/src/test/java/com/example/TemperatureConverterTest.java
+++ sm-maven/src/test/java/com/example/TemperatureConverterTest.java
@@ -1,6 +1,8 @@
 package com.example;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 import org.junit.jupiter.api.Test;
 
 import main.TemperatureConverter;
@@ -8,21 +10,26 @@ import main.TemperatureConverter;
 public class TemperatureConverterTest {
 
     @Test
-    public void fahrenheitToCelcius() {
+    public void testFahrenheitToCelcius() {
         assertEquals(-15, TemperatureConverter.fahrenheitToCelcius(5));
     }
 
     @Test
-    public void celciusToFahrenheit() {
+    public void testCelciusToFahrenheit() {
         assertEquals(48.2, TemperatureConverter.celciusToFahrenheit(9));
     }
 
     @Test
-    public void isExtremeTemperature() {
-        assertEquals(false, TemperatureConverter.isExtremeTemperature(-40));
-        assertEquals(true, TemperatureConverter.isExtremeTemperature(-41));
-        assertEquals(false, TemperatureConverter.isExtremeTemperature(20));
-        assertEquals(false, TemperatureConverter.isExtremeTemperature(50));
-        assertEquals(true, TemperatureConverter.isExtremeTemperature(51));
+    public void testIsExtremeTemperature() {
+        assertFalse(TemperatureConverter.isExtremeTemperature(-40));
+        assertTrue(TemperatureConverter.isExtremeTemperature(-41));
+        assertFalse(TemperatureConverter.isExtremeTemperature(20));
+        assertFalse(TemperatureConverter.isExtremeTemperature(50));
+        assertTrue(TemperatureConverter.isExtremeTemperature(51));
+    }
+
+    @Test
+    public void testOverallConversionAccuracy() {
+        assertEquals(100, TemperatureConverter.celciusToFahrenheit(TemperatureConverter.fahrenheitToCelcius(100)), 01);
     }
 }
